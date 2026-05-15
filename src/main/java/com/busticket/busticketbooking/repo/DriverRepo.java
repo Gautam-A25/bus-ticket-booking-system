7665454663d89db6ffebafb6304161d20db6ce65
ab4041 <138356134+ab4041@users.noreply.github.com>
2026-05-15 09:48:29 +0530

    [Anirudh]: Added Bus and Driver modules

---
+++ src/main/java/com/busticket/busticketbooking/repo/DriverRepo.java
@@ -0,0 +1,12 @@
+package com.busticket.busticketbooking.repo;
+
+import com.busticket.busticketbooking.entity.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DriverRepo extends JpaRepository<Driver, Integer> {
+
+    List<Driver> findByOffice_id(Integer officeId);
+
+}
---
+++ src/main/java/com/busticket/busticketbooking/service/impl/BusServiceImpl.java
@@ -0,0 +1,57 @@
+package com.busticket.busticketbooking.service.impl;
+
+import com.busticket.busticketbooking.entity.Bus;
+import com.busticket.busticketbooking.repo.BusRepo;
+import com.busticket.busticketbooking.service.BusService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BusServiceImpl implements BusService {
+
+    private final BusRepo busRepo;
+
+    public BusServiceImpl(BusRepo busRepo) {
+        this.busRepo = busRepo;
+    }
+
+    @Override
+    public Bus registerBus(Integer officeId, Bus bus) {
+
+        return busRepo.save(bus);
+    }
+
+    @Override
+    public List<Bus> getBusesByOffice(Integer officeId) {
+
+        return busRepo.findByOffice_id(officeId);
+    }
+
+    @Override
+    public Bus getBusById(Integer busId) {
+
+        return busRepo.findById(busId)
+                .orElseThrow(() -> new RuntimeException("Bus Not Found"));
+    }
+
+    @Override
+    public Bus updateBus(Integer busId, Bus bus) {
+
+        Bus existingBus = getBusById(busId);
+
+        existingBus.setRegistrationNumber(bus.getRegistrationNumber());
+        existingBus.setCapacity(bus.getCapacity());
+        existingBus.setType(bus.getType());
+
+        return busRepo.save(existingBus);
+    }
+
+    @Override
+    public void deleteBus(Integer busId) {
+
+        Bus bus = getBusById(busId);
+
+        busRepo.delete(bus);
+    }
+}
