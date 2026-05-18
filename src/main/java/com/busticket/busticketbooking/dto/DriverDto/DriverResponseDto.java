dac1ac14c8003605904c1d4fae79fe22ced338d7
ab4041 <138356134+ab4041@users.noreply.github.com>
2026-05-18 14:35:28 +0530

    [Anirudh] completed Driver module implementation and fixes

---
+++ src/main/java/com/busticket/busticketbooking/dto/DriverDto/DriverResponseDto.java
@@ -0,0 +1,65 @@
+package com.busticket.busticketbooking.dto.DriverDto;
+
+import java.lang.Integer;
+
+public class DriverResponseDto {
+
+    private Integer id;
+    private Integer officeId;
+    private Integer addressId;
+
+    private String licenseNumber;
+    private String name;
+    private String phone;
+
+    public DriverResponseDto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getOfficeId() {
+        return officeId;
+    }
+
+    public void setOfficeId(Integer officeId) {
+        this.officeId = officeId;
+    }
+
+    public Integer getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Integer addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
