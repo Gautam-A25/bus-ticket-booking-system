ca73517b3c2d401a2cd10fec3158f320c419faea
Gautam-A25 <aayushgautam2502@gmail.com>
2026-05-20 11:24:21 +0530

    [Aayush] : Added : Test DB configuration and test class bug fixes

--- src/test/java/com/busticket/busticketbooking/ExceptionHandlingTest.java
+++ src/test/java/com/busticket/busticketbooking/ExceptionHandlingTest.java
@@ -4,8 +4,10 @@ import com.busticket.busticketbooking.controller.BusController;
 import com.busticket.busticketbooking.controller.PaymentController;
 import com.busticket.busticketbooking.exception.GlobalExceptionHandler;
 import com.busticket.busticketbooking.exception.ResourceNotFoundException;
+import com.busticket.busticketbooking.security.CustomUserDetailsService;
 import com.busticket.busticketbooking.service.BusService;
 import com.busticket.busticketbooking.service.PaymentService;
+import com.busticket.busticketbooking.security.JwtService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +20,10 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 @WebMvcTest({BusController.class, PaymentController.class})
+@AutoConfigureMockMvc(addFilters = false)
 @Import(GlobalExceptionHandler.class)
 public class ExceptionHandlingTest {
 
@@ -31,6 +35,12 @@ public class ExceptionHandlingTest {
 
     @MockitoBean
     private PaymentService paymentService;
+
+    @MockitoBean
+    private JwtService jwtService;
+
+    @MockitoBean
+    private CustomUserDetailsService customUserDetailsService;
 
     @Test
     void shouldReturn404WhenBusNotFound() throws Exception {
--- src/test/java/com/busticket/busticketbooking/RepositoryTests.java
+++ src/test/java/com/busticket/busticketbooking/RepositoryTests.java
@@ -25,6 +25,7 @@ import com.busticket.busticketbooking.repo.TripRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -34,6 +35,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class RepositoryTests {
 
     @Autowired private AddressRepo addressRepo;
