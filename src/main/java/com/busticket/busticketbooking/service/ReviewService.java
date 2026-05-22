fc02c460b75fb2f54b3ce860b6b21f30c35d2b8b
Gautam-A25 <aayushgautam2502@gmail.com>
2026-05-22 14:55:11 +0530

    [Aayush] : Refactor : Bug fixes

--- src/main/java/com/busticket/busticketbooking/service/ReviewService.java
+++ src/main/java/com/busticket/busticketbooking/service/ReviewService.java
@@ -3,6 +3,7 @@ package com.busticket.busticketbooking.service;
 import com.busticket.busticketbooking.dto.ReviewDTO.ReviewRequestDTO;
 import com.busticket.busticketbooking.dto.ReviewDTO.ReviewResponseDTO;
 import org.springframework.data.domain.Page;
+
 import java.util.List;
 
 // Service interface defining all review business operations
@@ -11,4 +12,5 @@ public interface ReviewService {
     List<ReviewResponseDTO> getTripReviews(Integer tripId);
     List<ReviewResponseDTO> getCustomerReviews(Integer customerId);
     String removeReview(Integer reviewId);
+    Page<ReviewResponseDTO> getReviewPage(int page, int size);
 }
